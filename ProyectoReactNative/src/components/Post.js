712d16db5f45ee47927d97fd7b265855bc51968b
Luz Vedoya <lvedoya@udesa.edu.ar>
2021-11-11 10:18:47 -0300

    comentarios

--- ProyectoReactNative/src/components/Post.js
+++ ProyectoReactNative/src/components/Post.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Modal, TextInput} from 'react-native';
 import { db, auth } from '../firebase/config';
 import firebase from 'firebase';
+import { FlatList } from 'react-native-gesture-handler';
 
 class Post extends Component{
     constructor(props){
@@ -9,6 +10,8 @@ class Post extends Component{
         this.state = {
            likes: 0,
            myLike:false,
+           showModal: false,
+           comment: '',
         }
     }
     
@@ -48,23 +51,104 @@ class Post extends Component{
 
     }
 
+    showModal(){
+        this.setState({
+            showModal:true,
+        })
+    }
+
+    hideModal(){
+        this.setState({
+            showModal: false, 
+        })
+    }
+
+    guardarComentario(){
+        console.log('comentario')
+        let oneComment ={
+            createdAt: Date.now(),
+            author: auth.currentUser.email,
+            comment: this.state.comment,
+        }
+
+        db.collection('posts').doc(this.props.postData.id).update({
+            comments:firebase.firestore.FieldValue.arrayUnion(oneComment)
+         })
+         .then(()=>{
+             this.setState({
+                 comment: '' 
+             })
+         })
+    }
+
     render(){
         console.log(this.props);
         return(
             <View style={styles.contanier}>
              <Text>Texto del post: {this.props.postData.data.texto}</Text>
-             <Text>User: {this.props.postData.data.name} </Text>
-             <Text>User email: {this.props.postData.data.owner} </Text>
-             <Text>Likes: {this.state.likes} </Text>    
+             <Text>user: {this.props.postData.data.name} </Text>  
+             <Text>user email: {this.props.postData.data.owner} </Text>  
+             <Text>Likes: {this.state.likes} </Text> 
 
-            {/* if ternario que define si aparece dar like o sacar like */}
-            {this.state.myLike === false ?
-             <TouchableOpacity onPress={()=>this.darLike()}>
-                 <Text>Me gusta</Text>
-             </TouchableOpacity> :
-             <TouchableOpacity onPress={()=>this.quitarLike()}>
-                  <Text>Quitar Like</Text>
-              </TouchableOpacity> }
+             {/* Cambio de botones me gusta/ me dejó de gustar */}
+            {
+                this.state.myLike == false ?
+                <TouchableOpacity onPress={()=>this.darLike()}>
+                    <Text>Me gusta</Text>
+                </TouchableOpacity> :
+                <TouchableOpacity onPress={()=>this.quitarLike()}>
+                    <Text>Quitar like</Text>
+                </TouchableOpacity>                       
+            }
+            {/* Ver modal */}
+            <TouchableOpacity onPress={()=>this.showModal()}>
+                <Text>Ver Comentarios</Text>
+            </TouchableOpacity>
+
+            {/* Modal para los comentarios 
+            Lo pongo en un if ternario que se fija el valor de showModal del estado
+            */}
+            {   this.state.showModal ?
+                <Modal
+                    style= {styles.modalContainer}
+                    visible={this.state.showModal}
+                    animationType='slide'
+                    transparent={false}
+                >   
+                    <TouchableOpacity style={styles.closeButton} onPress={()=>this.hideModal()}>
+                        <Text>X</Text>
+                    </TouchableOpacity> 
+                    <Text>Comentarios:</Text>
+
+                    {/* Flatlist para mostrar los comentarios */}
+                    <FlatList
+                        data={this.props.postData.data.comments} //array de informacion que queremos mostrar
+                        keyextractor= {(comment)=>comment.createdAt.toString()} //paso la fecha de creacion a string
+                        renderItem={ ({item}) => <Text>{item.author}: {item.comment}</Text>}
+                    />
+
+
+                    {/* Formulario para nuevo comentarios */}
+                    <View>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder="Comentar..."
+                            keyboardType="default"
+                            multiline
+                            onChangeText={text => this.setState({comment: text})}
+                            value={this.state.comment}
+                        />
+                        {/* boton para que se guarde el comentario */}
+                        <TouchableOpacity style={styles.button} onPress={()=>{this.guardarComentario()}}>
+                            <Text style={styles.textButton}>Guadar comentario</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </Modal>    :
+                <Text></Text>
+            } 
+
+
             </View>
         )
     }
@@ -79,6 +163,46 @@ const styles = StyleSheet.create({
         borderColor: "#ccc",
         borderWidth: 1,
         padding: 10,
+    },
+    modalContainer:{
+        width: '97%',
+        borderRadius:4,
+        padding:5,
+        alignSelf: 'center',
+        boxShadow: 'rgb(204 204 204) 0px 0px 9px 7px', //no funciona en movile
+        marginTop: 20,
+        marginBottom: 10 
+    },
+    closeButton:{
+        color: '#fff',
+        padding: 5,
+        backgroundColor: '#dc3545',
+        alignSelf: 'flex-end',
+        borderRadius:4,
+        paddingHorizontal: 8,
+    },
+    input:{
+        height:40,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'#28a745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
     }
 })
 
