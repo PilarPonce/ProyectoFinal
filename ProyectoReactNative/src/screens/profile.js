ad89195738afe401c5b15fce752b29f7a6ec0b93
Martina <mtomlianovich@udesa.edu.ar>
2021-11-15 09:49:27 -0300

    posteos en profile

--- ProyectoReactNative/src/screens/profile.js
+++ ProyectoReactNative/src/screens/profile.js
@@ -1,22 +1,53 @@
 import React, {Component} from 'react';
+import { db, auth } from '../firebase/config';
 import {Text, TouchableOpacity, View, StyleSheet, Image, ActivityIndicator, FlatList, TextInput} from 'react-native';
+import Post from '../components/Post';
 
 class Profile extends Component{
   constructor(props){
     super(props);
     this.state ={
+      posteos: []
       
     }
   }
+
+  componentDidMount(){
+    db.collection('posts').where('owner', '==', auth.currentUser.email).onSnapshot(
+      docs => {
+        console.log(docs);
+        let posts = [];
+        docs.forEach( doc => {
+          posts.push({
+            id: doc.id,   
+            data: doc.data(),
+          })
+        })
+        console.log(posts);
+
+        this.setState({
+          posteos: posts,
+        })
+      }
+    )
+  }
+
   render(){
     return(
       <View style={styles.container}>
           <Text style={styles.welcome}> Username: {this.props.userData.displayName}</Text>
           <Text style={styles.welcome}> Mail: {this.props.userData.email}</Text>
           <Text style={styles.element}> Last login: {this.props.userData.metadata.lastSignInTime}</Text>
+          <View style={styles.containerPost}>
+          <FlatList 
+            data= { this.state.posteos }
+            keyExtractor = { post => post.id}
+            renderItem = { ({item}) => <Post postData={item} />} 
+          />
+          </View>
           <TouchableOpacity style={styles.touchable} onPress={()=>this.props.logout()}>
             <Text style={styles.touchableText}>Logout</Text>
-    </TouchableOpacity>
+          </TouchableOpacity>
       </View>       
     )
   }
@@ -46,6 +77,9 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color:'#fff',
         textAlign: 'center'
+    },
+    containerPost:{
+      paddingHorizontal:10,
     }
     
 });
